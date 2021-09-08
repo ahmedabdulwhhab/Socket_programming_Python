bb6325e4b21552e4846e6ac543ad846795fc1dcf
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2021-09-08 09:12:07 +0200

    Update server-chat.py

--- server-chat.py
+++ server-chat.py
@@ -1,9 +1,16 @@
 import socket
 
-print("single host, single server connection - waiting client to send message, Bye or bye to End")
-
-s = socket.socket()
 port = 12345
+"""
+manually confirm port is not used.
+#find the process using the port
+lsof -i :8000
+#and kill it
+kill -9 process_id
+"""
+print("waiting client to send message, Bye or bye to End")
+s = socket.socket()
+print("socket.socke()==",s)
 s.bind(('', port))
 s.listen(5)
 c, addr = s.accept()
@@ -11,8 +18,20 @@ print ("Socket Up and running with a connection from",addr)
 while True:
     rcvdData = c.recv(1024).decode()
     print ("S:",rcvdData)
-    sendData = input("N: ")
-    c.send(sendData.encode())
-    if(sendData == "Bye" or sendData == "bye"):
-        break
+    if(rcvdData=='t'):
+        from datetime import datetime
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        c.send(current_time.encode())
+    elif(rcvdData=='d'):
+        from datetime import date
+        today = date.today()
+        #dd/mm/YY
+        d1 = today.strftime("%d/%m/%Y")
+        c.send(d1.encode())
+    else:
+        sendData = input("N: ")
+        c.send(sendData.encode())
+        if(sendData == "Bye" or sendData == "bye"):
+            break
 c.close()
