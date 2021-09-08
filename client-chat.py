12f618a9159498362355d3ecce9b51e82ff10302
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2021-09-08 08:19:22 +0200

    Add files via upload

---
+++ client-chat.py
@@ -0,0 +1,12 @@
+import socket
+
+s = socket.socket()
+s.connect(('127.0.0.1',12345))
+while True:
+    str = input("t for time, d for date , IP for ip address: ")        #python 3
+    #str = raw_input("S: ")      #python 2
+    s.send(str.encode());
+    if(str == "Bye" or str == "bye"):
+        break
+    print ("N:",s.recv(1024).decode())
+s.close()
