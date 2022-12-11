f54a4445a24f64de57f2ac6a033952f248927f1d
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2022-12-11 07:19:37 +0200

    Add files via upload

---
+++ Scripts/client_in_finite_loop_2.py
@@ -0,0 +1,40 @@
+import socket
+from time import sleep
+print("send message, Bye or bye to End")
+s = socket.socket()
+s.connect(('10.0.0.1',12346))
+while True:
+    #str = input(" ip_1 for h1  for ip address: ")        #python 3
+    #str = raw_input("S: ")      #python 2
+    str="t"
+    s.send(str.encode());
+    received_data=s.recv(1024).decode()
+    print ("received data",received_data)
+    sleep(3)
+    ###########
+    str="d"
+    s.send(str.encode());
+    received_data=s.recv(1024).decode()
+    print ("received data",received_data)
+    sleep(3)
+    ###########
+    str="ip_1"
+    s.send(str.encode());
+    received_data=s.recv(1024).decode()
+    print ("received data",received_data)
+    sleep(3)
+    ###########
+    str="ip_2"
+    s.send(str.encode());
+    received_data=s.recv(1024).decode()
+    print ("received data",received_data)
+    sleep(3)    
+    ###########
+    str="ip_3"
+    s.send(str.encode());
+    received_data=s.recv(1024).decode()
+    print ("received data",received_data)
+    sleep(3) 
+
+s.close()
+
